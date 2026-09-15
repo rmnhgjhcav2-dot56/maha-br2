402dc2d9ce671913d61210d6762202dd405a9054
rmnhgjhcav2-dot56 <rrrddd92@gmail.com>
2026-09-15 13:53:08 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,5 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import BridalApp from "./BridalApp.jsx";
 
+createRoot(document.getElementById("root")).render(<BridalApp />);
